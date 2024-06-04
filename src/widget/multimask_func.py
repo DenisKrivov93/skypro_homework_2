5d2b6ff20fae9108ad6be4673d26345e84b2215c
Krivov Denis <d.krivov93@gmail.com>
2024-06-05 00:47:43 +0800

    Довнесение правок

--- src/widget/multimask_func.py
+++ src/widget/multimask_func.py
@@ -3,23 +3,14 @@ from src.masks import account_mask, card_mask
 
 def multimask_func(user_input: str) -> str:
     """Выявляет какие данные ей предоставили и возвращает данные на основе заготовленных функций с масками"""
-    key_word_account = "Счет"
-    key_word_visa_classic = "Visa Classic"
-    key_word_visa_platinum = "Visa Platinum"
-    key_word_visa_gold = "Visa Gold"
-    key_word_maestro = "Maestro"
-    key_word_mastercard = "MasterCard"
-    if key_word_account in user_input:
-        return "Счет " + account_mask(user_input[-4:])
-    elif key_word_visa_classic in user_input:
-        return "Visa Classic " + card_mask(user_input[-16:])
-    elif key_word_visa_platinum in user_input:
-        return "Visa Platinum " + card_mask(user_input[-16:])
-    elif key_word_visa_gold in user_input:
-        return "Visa Gold " + card_mask(user_input[-16:])
-    elif key_word_maestro in user_input:
-        return "Maestro " + card_mask(user_input[-16:])
-    elif key_word_mastercard in user_input:
-        return "MasterCard " + card_mask(user_input[-16:])
+    key_words_cards = ["Visa Classic", "Visa Gold", "Maestro", "MasterCard"]
+    payment_info = user_input.split(" ")
+    payment_number = payment_info[-1]
+    payment_name = " ".join(payment_info[:-1])
+    if payment_name == "Счет":
+        payment_number = account_mask(payment_number)
+    elif payment_name in key_words_cards:
+        payment_number = card_mask(payment_number)
     else:
         return "Приложение не работает с выбранным банком или введена некорректая информация"
+    return f"{payment_name} {payment_number}"
--- tests/tests.py
+++ tests/tests.py
@@ -1,8 +1,19 @@
-from datetime import datetime
+from src.masks import account_mask, card_mask
 
 
-def date_func(date: str) -> str:
-    return datetime.fromisoformat(date).strftime('%d.%m.%Y')
+def multimask_func(user_input: str) -> str:
+    """Выявляет какие данные ей предоставили и возвращает данные на основе заготовленных функций с масками"""
+    key_words_cards = ["Visa Classic", "Visa Gold", "Maestro", "MasterCard"]
+    payment_info = user_input.split(" ")
+    payment_number = payment_info[-1]
+    payment_name = " ".join(payment_info[:-1])
+    if payment_name == "Счет":
+        payment_number = account_mask(payment_number)
+    elif payment_name in key_words_cards:
+        payment_number = card_mask(payment_number)
+    else:
+        return "Приложение не работает с выбранным банком или введена некорректая информация"
+    return f"{payment_name} {payment_number}"
 
 
-print(date_func("2018-07-11T02:26:18.671407"))
+print(multimask_func("Maestro 1596837868705199"))
