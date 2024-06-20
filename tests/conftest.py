85270fb7d575a8918236512512e5452541bdf246
Krivov Denis <d.krivov93@gmail.com>
2024-06-20 20:49:10 +0800

    внесены очередные правки по заданию 10.2

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,10 +2,10 @@ import pytest
 
 
 @pytest.fixture
-def account_fixture():
+def account_fixture() -> str:
     return "**3210"
 
 
 @pytest.fixture
-def card_fixture():
+def card_fixture() -> str:
     return "1234 56** **** 3210"
--- tests/test_date_func.py
+++ tests/test_date_func.py
@@ -1,5 +1,7 @@
+from typing import Any
+
 from src.widget.date_func import date_func
 
 
-def test_date_func():
+def test_date_func() -> Any:
     assert date_func("2019-07-03T18:35:29.512364") == "03.07.2019"
